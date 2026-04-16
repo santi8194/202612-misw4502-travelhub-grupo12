5c41681a3cd6b6c51f0b364447c1a0c82e7f6d88
drvillota <dr.villota@uniandes.edu.co>
2026-04-15 20:15:05 -0500

    Add devops to gh web pages deploy

--- Frontend/user-web/src/app/app.config.ts
+++ Frontend/user-web/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([errorInterceptor])),
   ]
 };

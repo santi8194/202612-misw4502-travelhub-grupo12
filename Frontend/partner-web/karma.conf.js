af5e621134cf3008d380253e876fff998c751d83
Santiago Alvarez Soto <alvarezsoto13@gmail.com>
2026-04-26 23:59:12 -0500

    chore(worflows): Enhance CI configurations and add code coverage checks for backend and frontend

--- Frontend/partner-web/karma.conf.js
+++ Frontend/partner-web/karma.conf.js
@@ -1,29 +1,38 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine'],
+    frameworks: ["jasmine"],
     plugins: [
-      'karma-jasmine',
-      'karma-chrome-launcher',
-      'karma-coverage',
-      'karma-jasmine-html-reporter',
+      "karma-jasmine",
+      "karma-chrome-launcher",
+      "karma-coverage",
+      "karma-jasmine-html-reporter",
     ],
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ["ChromeHeadlessNoSandbox"],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage',
-        ],
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/partner-web'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'text' }, { type: 'json-summary' }],
+      dir: require("path").join(__dirname, "./coverage/partner-web"),
+      subdir: ".",
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "text" },
+        { type: "json-summary" },
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        },
+      },
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     restartOnFileChange: true,
   });
 };

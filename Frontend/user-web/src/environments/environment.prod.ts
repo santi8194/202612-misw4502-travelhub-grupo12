67a4a2e84580dc480e970b27e1a633e5fa8ad5b2
Orlando Giovanny Solarte Delgado <o.solarte@uniandes.edu.co>
2026-05-02 10:58:24 -0500

    Se refactoriza nombre de searchapi en user-web

--- Frontend/user-web/src/environments/environment.prod.ts
+++ Frontend/user-web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://d1d660udfb1fc0.cloudfront.net/search/api',
+  searchApiUrl: 'https://d1d660udfb1fc0.cloudfront.net/search/api',
   bookingApiUrl: 'https://d1d660udfb1fc0.cloudfront.net/booking/api/reserva',
   catalogApiUrl: 'https://d1d660udfb1fc0.cloudfront.net/catalog',
   paymentApiUrl: 'https://d1d660udfb1fc0.cloudfront.net/payment',
--- Frontend/user-web/src/environments/environment.ts
+++ Frontend/user-web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5001/search/api',
+  searchApiUrl: 'http://localhost:5001/search/api',
   bookingApiUrl: 'http://localhost:5001/booking/api/reserva',
   catalogApiUrl: 'http://localhost:5001/catalog',
   paymentApiUrl: 'http://localhost:5001/payment',
